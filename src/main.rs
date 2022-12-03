4a84df212131ddadb628b74ed145770732201cc0
Spencer Waguespack <wswaguespack403@gmail.com>
2022-12-03 14:42:24 -0500

    does the thing

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,27 @@ fn main() {
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why)=> panic!("could read {}: {}",display,why),
-        Ok(_)=> print!("contains:data\n{}",s ),
+        Ok(_)=> print!("contains:\n" ),
     }
 
 
     let split_sting = s.split("\n");
 
     let contents = split_sting.collect::<Vec<&str>>();
+    println!();
+    println!();
 
+    let mut total_value =0;
     for cont in contents{
-        get_first_half(cont);
+        println!("{}",cont);
+        let container1 = get_first_half(cont);
+        let container2 = get_second_half(cont);
+        total_value = total_value + get_matching_data(&container1, &container2);
+
+        println!();
     }
+
+    println!("total value {}",total_value);
 }
 
 fn get_first_half(target: &str ) -> String{
@@ -38,4 +48,42 @@ fn get_first_half(target: &str ) -> String{
     println!("{}", items);
     return items;
     
+}
+fn get_second_half(target: &str ) -> String{
+    let mut items = String::new();
+    for (i,ch) in target.chars().enumerate(){
+        //println!("{} {}", i, ch);
+        if i >= target.len()/2 {
+            items.push(ch);
+        }
+    }
+    println!("{}", items);
+    return items;
+    
+}
+
+fn get_matching_data(comp1: &str,comp2: &str) ->i32{
+    let mut prev_match = String::new();
+    let mut values = 0; 
+    for ch in comp1.chars()
+    {
+        if comp2.contains(ch) && !prev_match.contains(ch){
+            println!("match {}",ch);
+            values = get_value(ch);
+            prev_match.push(ch);
+        }
+    }
+    return values;
+}
+
+fn get_value(item: char) -> i32 {
+    let mut val = 0;
+    if item.is_ascii_uppercase(){
+        val = item as i32 - 65 + 27;
+        println!("{}", val);
+    }else if item.is_ascii_lowercase() {
+        val = item as i32 - 96;
+        println!("{}", val);
+    }
+    return val;
 }
