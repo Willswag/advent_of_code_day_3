c8a44e657bc096869c2278f8f3d6a6b736722ee2
Spencer Waguespack <wswaguespack403@gmail.com>
2022-12-03 13:41:03 -0500

    function to get the first half

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("input.txt");
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldnt open {}: {}",display,why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why)=> panic!("could read {}: {}",display,why),
+        Ok(_)=> print!("contains:data\n{}",s ),
+    }
+
+
+    let split_sting = s.split("\n");
+
+    let contents = split_sting.collect::<Vec<&str>>();
+
+    for cont in contents{
+        get_first_half(cont);
+    }
+}
+
+fn get_first_half(target: &str ) -> String{
+    let mut items = String::new();
+    for (i,ch) in target.chars().enumerate(){
+        //println!("{} {}", i, ch);
+        if i < target.len()/2 {
+            items.push(ch);
+        }
+    }
+    println!("{}", items);
+    return items;
+    
 }
